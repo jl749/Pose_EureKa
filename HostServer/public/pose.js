38995b691e9e65a546e6734a822037b7c72995c9
jl749 <wjdan7051@gmail.com>
2021-11-23 19:28:27 +0900

    pose algorithm update

--- HostServer/public/pose.js
+++ HostServer/public/pose.js
@@ -32,71 +32,118 @@ function normaliseCor(input) {
 }
 
 function poseClassification(cors) {    
-//     visibleSide = (cor1, cor2) => {
-//         if(!cor1)
-//             return (!cor2) ? null : cor2;
-//         else
-//             return cor1;
-//     }
-//     let shoulder = visibleSide(cors[11], cors[12]);
-    let shoulder;
-    if(!cors[11])
-        shoulder = (!cors[12]) ? null : cors[12];
-    else
-        shoulder = cors[11];
-    
-    let wrist;
-    if(!cors[15])
-        wrist = (!cors[16]) ? null : cors[16];
-    else
-        wrist = cors[15];
-    
-    let hip;
-    if(!cors[23])
-        hip = (!cors[24]) ? null : cors[24];
-    else
-        hip = cors[23];
-    
-    let knee;
-    if(!cors[25])
-        knee = (!cors[26]) ? null : cors[26];
-    else
-        knee = cors[25];
-
     if(!cors[0])
         return 'error';
+
+    visibleSide = (cor1, cor2) => {
+        if(!cor1)
+            return (!cor2) ? null : cor2;
+        else
+            return cor1;
+    };
+
+    let shoulder = visibleSide(cors[11], cors[12]);
+    let wrist = visibleSide(cors[15], cors[16]);
+    let hip = visibleSide(cors[23], cors[24]);
+    let knee = visibleSide(cors[25], cors[26]);
 
     bothArmForward = () => {
         if( (cors[15].x > cors[0].x && cors[16].x > cors[0].x) || (cors[15].x < cors[0].x && cors[16].x < cors[0].x) )
             return true;
         return false;
+    };
+    oneArmHigherThanNose = () => {
+        if( (cors[15].y < cors[0].y && cors[13].y < cors[0].y) || (cors[16].y < cors[0].y && cors[14].y < cors[0].y) )
+            return true;
+        return false;
+    };
+    x_corBetween = (cor1, cor2, cor3) => {
+        if( cor1.x < cor2.x && cor2.x < cor3.x )
+            return true;
+        return false;
+    };
+    // facingRight = () => {
+    //     if( Math.abs(cors[1].x) < Math.abs(cors[4].x))
+    //         return true;  // facing right
+    //     return false;  // facing left
+    // };
+
+    // jump
+    if(cors[15]&&cors[13]&&cors[16]&&cors[14]&& (cors[12] && cors[11]) ) {
+        if( oneArmHigherThanNose() && (x_corBetween(cors[16], cors[12], cors[0]) || x_corBetween(cors[0], cors[11], cors[15])) )
+            return 'jump';
     }
 
-    // console.log(cors[0], cors[13], cors[15]);
-    // skill
-    if(cors[15]&&cors[16]&&shoulder&&hip) { 
-        if( ((cors[15].y < shoulder.y && cors[16].y > shoulder.y && cors[16].y < hip.y) || (cors[16].y < shoulder.y && cors[15].y > shoulder.y && cors[15].y < hip.y)) && bothArmForward() )
-            return 'skill';
+    //kickL
+    if(cors[24] && cors[25] && cors[26] && cors[28]) {
+        if((cors[26].y < cors[25].y) && (cors[28].y < cors[25].y))
+            return "kickL";
     }
-    // jump
-    if(cors[15]&&cors[13]&&cors[16]&&cors[14]&& (cors[12] || cors[11]) ) {
-        if( (cors[15].y < cors[0].y && cors[13].y < cors[0].y && cors[16].x < cors[12].x && cors[12].x < cors[0].x) || (cors[16].y < cors[0].y && cors[14].y < cors[0].y && cors[15].x > cors[11].x && cors[11].x > cors[0].x) ) {
-            return 'jump';
+    //kickR
+    if(cors[23] && cors[25] && cors[26] && cors[27]) {
+        if((cors[25].y< cors[23].y) && (cors[27].y < cors[26].y))
+            return "kickR";
+    }
+
+    //punch
+    let midpointy;
+    if(cors[11] && cors[13] && cors[15] && cors[23])
+    {
+        midpointy = (cors[11].y + cors[23].y) / 2
+        if(((cors[0].x>cors[13].x && cors[13].x >cors[15].x && calAngle(cors[11],cors[13],cors[15]) > 175) ||(cors[0].x < cors[13].x && cors[13].x<cors[15].x && calAngle(cors[11],cors[13],cors[15])> 160)) && (cors[11].y < cors[15].y) && (midpointy > cors[15].y))
+        {
+          return "punchR";
         }
     }
+    if(cors[12]&&cors[14]&&cors[16]&& cors[24])
+    {   midpointy = (cors[12].y + cors[24].y) / 2
+
+        if(((cors[0].x>cors[14].x && cors[14].x >cors[16].x && calAngle(cors[12],cors[14],cors[16]) > 175 ) ||(cors[0].x < cors[14].x && cors[14].x<cors[16].x && calAngle(cors[12],cors[14],cors[16])> 160)) && (cors[12].y < cors[16].y) && (midpointy > cors[16].y))
+        {
+          return "punchL";
+        }
+    }
+
+    //backward
+    if(cors[11]&&cors[12]&&cors[13]&&cors[14]&&cors[15]&&cors[16]&&cors[23]&&cors[24]){
+        if((cors[15].y < cors[11].y && cors[11].y<cors[13].y && cors[13].y<cors[23].y) && (cors[16].y < cors[12].y && cors[12].y < cors[14].y && cors[14].y <cors[24].y))
+        {
+            return "backward";
+        }
+    }
+    // forward
+    // if(cors[11]&&cors[12]&&cors[27]&&cors[28])
+    // {
+    //     let shoulder_len = cors[11].x - cors[12].x
+    //     let ankle_len = cors[27].x - cors[28].x
+    //     if((cors[28].x < cors[12].x && cors[12].x < cors[11].x && cors[11].x < cors[27].x) || (cors[28].x > cors[12].x && cors[12].x > cors[11].x && cors[11].x > cors[27].x) && (shoulder_len< ankle_len))
+    //     {
+    //         return "forward";
+    //     }
+    // }
+    //forward
+    if(cors[11]&&cors[12]&&cors[25]&&cors[26]&&cors[27]&&cors[28])
+    {
+        let shoulder_len = cors[11].x - cors[12].x
+        let ankle_len = cors[27].x - cors[28].x
+        if((cors[28].x<cors[26].x && cors[26].x < cors[12].x && cors[12].x < cors[11].x && cors[11].x < cors[25].x && cors[25].x < cors[27].x) && (shoulder_len< ankle_len))
+        {
+            return "forward";
+        }
+    }
+
     // crouch
     if(wrist&&knee) {
         if( wrist.y > knee.y) {
             return 'crouch';
         }
     }
-    if(cors[24]&&cors[26]&&cors[28]&&cors[23]&&cors[25]&&cors[27]) {
-        console.log(calAngle(cors[24], cors[26], cors[28]), calAngle(cors[23], cors[25], cors[27]));
+
+    // skill
+    if(cors[15]&&cors[16]&&hip&&shoulder) { 
+        if( ((cors[15].y < cors[0].y && cors[16].y > shoulder.y && cors[16].y < hip.y) || (cors[16].y < cors[0].y && cors[15].y > shoulder.y && cors[15].y < hip.y)) && bothArmForward() )
+            return 'skill';
     }
-    // forward
-    // else if( (cors[25].y < cors[26].y && cors[25].y < cors[28].y) || (cors[26].y < cors[25].y && cors[26].y < cors[27].y) ) {
-    //     label = 'forward';
-    // }
 
     
     return "unknown";
